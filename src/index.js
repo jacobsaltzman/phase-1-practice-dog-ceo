60d44cbf8616754c8c00fff7ced4c1a6d8dba06d
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-30 14:21:06 -0700

    commit tree 1226eb4943a68f3d1525cf6268d81538c2208cfc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+// "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener("DOMContentLoaded", (event) =>{
+  //console.log("dom loaded!")
+  //console.log(event)
+  getBreeds()
+  getBreedNames()
+})
+
+function getBreeds(){
+  fetch ("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(resp => {
+      //console.log(resp.message)
+      const dogImageContainer = document.getElementById("dog-image-container")
+      //console.log(dogImageContainer)
+      resp.message.forEach((url)=>{
+        const img = document.createElement("img")
+        img.src = url
+        dogImageContainer.append(img)
+      }
+       )
+    })
+}
+
+
+function getBreedNames(){
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(resp => {
+      //console.log(resp)
+      breeds = Object.keys(resp.message)
+      addBreedToDom(breeds)
+    })
+}
+document.addEventListener("click", event => {
+  if (event.target.matches("li")){
+    event.target.style.color = "green"
+  }
+})
+
+document.addEventListener("change", event => {
+ 
+  if(event.target.matches("#breed-dropdown")){
+    const ul = document.querySelector("#dog-breeds")
+    ul.innerHTML = ""
+    const filteredBreeds = breeds.filter(breed => breed[0] === event.target.value)
+    addBreedToDom(filteredBreeds)
+  }
+})
+
+function addBreedToDom(breeds){
+  const ul = document.querySelector("#dog-breeds")
+      breeds.map(breed => {
+        const li = document.createElement("li")
+        li.textContent = breed
+        ul.append(li)
+      })
+}
